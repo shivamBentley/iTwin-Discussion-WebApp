976854f31a6fd73d095007a432836a14a8b1e797
shivam Chaurasiya <shivam.chaurasiya@bentley.com>
2023-05-23 14:28:34 +0530

    filter by developer login id and filter by type

--- src/helper/util.js
+++ src/helper/util.js
@@ -107,3 +107,110 @@ export const GetNoRepliedData = (discussionData) => {
     // console.log('No Replied data', filteredData)
     return filteredData;
 }
+/**
+ * 
+ * @param {Array} discussionData 
+ * @returns {Set} All member login id. 
+ */
+export const getMembers = (discussionData) => {
+    const members = new Set();
+    discussionData.forEach((obj) => {
+        const comments = obj.comments?.nodes;
+        members.add(obj.author?.DeveloperQuestioned);
+        if (comments && comments.length != 0) {
+            comments.forEach((comment) => {
+                members.add(comment.author?.DeveloperCommented);
+            })
+        }
+    })
+    // console.log('Commented or replied member', members)
+    return members;
+}
+
+
+/**
+ * @param {Array<string>} Array of login id name;
+ * @returns { Array<Object> } Array of Discussion data
+ */
+export const getFilteredDataByDeveloperAndStatusType = (discussionData, gitHubLoginIds, filterType) => {
+
+    //filter by filterType - { commented, replies, answer }
+    let filterByType = [];
+    if (filterType.length === 0) {
+        filterByType = filterByType.concat(discussionData);
+    } else {
+        discussionData.forEach((data) => {
+            filterType.forEach((filter) => {
+                if (filter === 'comments') {
+                    if (data.comments && data.comments.totalCount !== 0 && !data.answer?.isAnswer)
+                        filterByType.push(data);
+                }
+                else if (filter === 'answer') {
+                    if (data.answer && data.answer.totalCount !== 0)
+                        filterByType.push(data);
+                }
+                else if (filter === 'replies') {
+                    const commentsData = data.comments?.nodes;
+                    if (commentsData && commentsData.totalCount !== 0) {
+                        commentsData.forEach((commentObj) => {
+                            if (commentObj.replies?.totalCount !== 0)
+                                filterByType.push(data);
+                        })
+                    }
+                }
+            })
+        })
+    }
+
+    let filteredDataByLoginIds = [];
+
+    // filter by loginIds 
+    if (gitHubLoginIds.length === 0) { filteredDataByLoginIds = filteredDataByLoginIds.concat(filterByType); }
+    else {
+        filterByType.forEach((data) => {
+
+            let answeredBy = '';
+            let commentsBy = [];
+            let repliedBy = [];
+
+            // check answer by developer exist
+            const answer = data.answer;
+            if (answer && answer.isAnswer) {
+                answeredBy = answer.author?.DeveloperAnswered
+            }
+
+            // developer commented 
+            const comments = data.comments.nodes;
+            if (comments && data.comments.totalCount !== 0) {
+                comments.forEach((comment) => {
+                    commentsBy.push(comment.author?.DeveloperCommented);
+
+                    //developer replied on comments
+                    const replies = comment.nodes;
+                    if (replies && comment.totalCount) {
+                        repliedBy.push(replies.author.DeveloperRepliedToComment);
+                    }
+
+                })
+            }
+
+            // if any gitHubLoginIds matches with answeredBy, commentsBy or repliedBy
+            gitHubLoginIds.forEach((loginId) => {
+                if (loginId === answeredBy) filteredDataByLoginIds.push(data);
+                else if (commentsBy.length !== 0) {
+                    commentsBy.forEach((commentBy) => {
+                        if (commentBy === loginId) filteredDataByLoginIds.push(data);
+                    })
+                }
+
+                else if (repliedBy.length !== 0) {
+                    repliedBy.forEach((replyBy) => {
+                        if (replyBy === loginId) filteredDataByLoginIds.push(data);
+                    })
+                }
+            })
+        })
+    }
+
+    return filteredDataByLoginIds;
+}
