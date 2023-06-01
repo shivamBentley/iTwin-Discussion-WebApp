7380e15f25818e6257941c3f036dcedbc7c9c020
shivam Chaurasiya <shivam.chaurasiya@bentley.com>
2023-06-01 19:41:13 +0530

    Logger added for redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducers';
+import {logger} from 'redux-logger'
 
 // ==============================|| REDUX TOOLKIT - MAIN STORE ||============================== //
 
 const store = configureStore({
     reducer: reducers,
+    middleware: [logger]
 });
 
 const { dispatch } = store;
