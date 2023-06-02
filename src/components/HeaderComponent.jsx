2ee35a50d606448216a7e0f0c7e614917b117147
shivam Chaurasiya <shivam.chaurasiya@bentley.com>
2023-06-03 00:03:10 +0530

    code for header component simplyfied

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -3,6 +3,7 @@ import { Title } from '@itwin/itwinui-react';
 import { useSelector } from 'react-redux';
 import { FilterModal } from './Filter/FilterModal';
 import ExportToExcel from './ExportToExcel';
+import './styles/HeaderComponent.scss'
 import './styles/ExportToExcel.scss'
 
 function HeaderComponent({ title }) {
@@ -10,53 +11,16 @@ function HeaderComponent({ title }) {
     const repoName = useSelector((state) => state.discussions.repositoryName);
     const lastUpdated = useSelector((state) => state.discussions.lastUpdated);
     const rateLimit = useSelector((state) => state.discussions.rateLimit)
+
     return (
-        <div style={{ position: 'relative', height: '100%' }}>
-            <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: '100%',
-                backgroundColor: 'lightgray',
-                padding: '10px 0 0 0'
-            }}>
-
-                <Title style={{ fontSize: '2rem', fontWeight: '400' }}>{repoName}</Title>
-
-                <div style={{ position: 'absolute', bottom: '10px', right: '100px' }}>
-                    <FilterModal />
-                </div>
-                <div style={{ position: 'absolute', bottom: '10px', right: '10px' }}>
-                    <ExportToExcel />
-                </div>
-                <div style={{ 
-                    position: 'absolute',
-                    top: '10px',
-                    right: '10px',
-                    border: '1px solid white',
-                    borderRadius: '3px',
-                    padding: '1px 10px',
-                    fontSize: '1rem',
-                    fontWeight: '600',
-                    backgroundColor: 'green',
-                    color: '#d8fafa'
-                    }}>
-                    <>{new Date(lastUpdated).toLocaleString()}</>
-                </div>
-                <div style={{
-                    position: 'absolute',
-                    bottom: '10px',
-                    left: '10px',
-                    border: '1px solid white',
-                    borderRadius: '3px',
-                    padding: '1px 10px',
-                    fontSize: '1rem',
-                    fontWeight: '600',
-                    backgroundColor: 'red',
-                    color: '#d8fafa'
-                }}>
-                    <>Remaining Points: {rateLimit.remaining}</>
-                </div>
+        <div className='header-main' >
+            <div className='header-parent'>
+                <Title className='header-title'>{repoName.charAt(0).toUpperCase() + repoName.slice(1)}</Title>
+                <div className='header-child filter-modal'><FilterModal /></div>
+                <div className='header-child download'><ExportToExcel /></div>
+                <div className='header-child last-update'><>{new Date(lastUpdated).toLocaleString()}</></div>
+                <div className='header-child remaining'><>Remaining Points: {rateLimit.remaining}</></div>
+                <div className='header-child reset-time'><>Reset Time: {new Date(rateLimit.resetAt).toLocaleString().substr(11, 10)}</></div>
             </div>
         </div>
     )
